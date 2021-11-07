2938f396533ea96d307c92418173626b2a7aa09f
Beneba <afetsib@gmail.com>
2021-11-07 06:50:34 +0000

    Accout page UI

--- App.js
+++ App.js
@@ -1,21 +1,85 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.profileContainer}>
+        <Image source ={require("./assets/Benedict.jpg")}  style ={ styles.profileImage}/>
+        <TextInput    placeholder={"Account"}  />
       <StatusBar style="auto" />
+    </View>
+    
+     <View style={styles.inputContainer}>
+    <TextInput  style={styles.textInput}  placeholder={"UserName"} />
+    <TextInput  style={styles.textInput}  placeholder={"Email"} /> 
+    <TextInput  style={styles.textInput}  placeholder={"Password"} />
+     
+   </View>
+
+    <View style={styles.buttonContainer}>
+      <TouchableOpacity  style ={styles.submitContainer} >
+        <Text>Submit</Text>
+      </TouchableOpacity>
+    </View>
+
     </View>
   );
 }
-
+///style goes here
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex:4,
+    backgroundColor: 'white',
+    
+  },
+  profileContainer:{
+    flex:4,
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 50
   },
+
+  inputContainer:  {
+    flex:4,
+    backgroundColor: 'white',
+    
+    justifyContent: 'center',
+  },
+
+  buttonContainer: {
+    flex:2,
+   
+    alignItems: 'center',
+   
+  },
+  profileImage: {
+    height:"70%",
+    width: "50%",
+    borderRadius: 50
+    
+  },
+  textInput: {
+    borderWidth: 2,
+    borderColor: "black",
+    padding: 10,
+    marginBottom:6,
+    padding:10,
+    margin: 15,
+  },
+  
+  submitContainer: {
+    color: "white",
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+    borderColor: "blue",
+   
+    borderWidth: 5,
+    marginTop:10,
+    padding:10,
+
+  }
 });
